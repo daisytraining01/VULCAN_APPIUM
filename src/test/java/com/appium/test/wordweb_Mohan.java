8d3707038677ed6d626a2ba4da312d0847b73f85
Mohan <mohan@zentpeople.com>
2020-05-22 13:00:42 +0530

    Mohan Files Added

--- src/test/java/com/appium/test/wordweb_Mohan.java
+++ src/test/java/com/appium/test/wordweb_Mohan.java
@@ -2,7 +2,7 @@ package com.appium.test;
 
 import org.testng.annotations.Test;
 
-import com.appium.pageObjects.pageObjects;
+import com.appium.pageObjects.pageObjects_Mohan;
 import com.beust.jcommander.Parameter;
 import com.helper.ExcelUtil;
 import com.helper.UserActions;
@@ -10,6 +10,8 @@ import com.listner.TestListener;
 
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.touch.offset.PointOption;
 
@@ -33,7 +35,7 @@ import org.testng.annotations.BeforeClass;
 
 
 
-public class EriBank  {
+public class wordweb_Mohan  {
 	
 
 	UserActions user;
@@ -55,20 +57,24 @@ public class EriBank  {
 			desiredCapabilities.setCapability("platformVersion", "10");
 			desiredCapabilities.setCapability("udid", "RZ8M90DTVRN");
 			desiredCapabilities.setCapability("deviceName", "Samsung");
-			desiredCapabilities.setCapability("appPackage", "com.experitest.ExperiBank");
-			desiredCapabilities.setCapability("appActivity", ".LoginActivity");
+			desiredCapabilities.setCapability("appPackage", "com.wordwebsoftware.android.wordweb");
+			desiredCapabilities.setCapability("appActivity", "com.wordwebsoftware.android.wordweb.activity.WordWebActivity");
+			desiredCapabilities.setCapability("unicodeKeyboard", "true");
+			desiredCapabilities.setCapability("resetKeyboard", "true");
 			URL remoteUrl = new URL("http://localhost:4723/wd/hub");
 			driver = new AndroidDriver(remoteUrl, desiredCapabilities);
 			user= new UserActions(driver);
 			
 		} else if (Device.contentEquals("Cloud_Device")) {
 			System.out.println("Entering Cloud device");
-			desiredCapabilities.setCapability("testName", "Mani");
+			desiredCapabilities.setCapability("testName", "WordWeb");
 			accessKey = "eyJ4cC51Ijo0NTU2OTcsInhwLnAiOjQ1NTY4OSwieHAubSI6Ik1UVTRPVE0yTnprMU1EQXlNdyIsImFsZyI6IkhTMjU2In0.eyJleHAiOjE5MDQ3Mjc5NTAsImlzcyI6ImNvbS5leHBlcml0ZXN0In0.9PEKvslTXNVQjibm_oLIBni8iknsIwo1etQfjBMQ8ME";
 			desiredCapabilities.setCapability("accessKey", accessKey);
 			desiredCapabilities.setCapability("deviceQuery", "@os='android' and @category='PHONE'");
-			desiredCapabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.experitest.ExperiBank");
-			desiredCapabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, ".LoginActivity");
+			desiredCapabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.wordwebsoftware.android.wordweb");
+			desiredCapabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.wordwebsoftware.android.wordweb.activity.WordWebActivity");
+			desiredCapabilities.setCapability("unicodeKeyboard", "true");
+			desiredCapabilities.setCapability("resetKeyboard", "true");
 			driver = new AndroidDriver(new URL("https://demo.experitest.com/wd/hub"), desiredCapabilities);
 			user= new UserActions(driver);
 		}else if (Device.contentEquals("Emulator_Device")) {
@@ -77,60 +83,84 @@ public class EriBank  {
 			desiredCapabilities.setCapability("platformVersion", "9");
 			desiredCapabilities.setCapability("udid", "emulator-5554");
 			desiredCapabilities.setCapability("deviceName", "Emulator");
-			desiredCapabilities.setCapability("appPackage", "com.experitest.ExperiBank");
-			desiredCapabilities.setCapability("appActivity", ".LoginActivity");
+			desiredCapabilities.setCapability("appPackage", "com.wordwebsoftware.android.wordweb");
+			desiredCapabilities.setCapability("appActivity", "com.wordwebsoftware.android.wordweb.activity.WordWebActivity");
+			desiredCapabilities.setCapability("unicodeKeyboard", "true");
+			desiredCapabilities.setCapability("resetKeyboard", "true");
 
-			URL remoteUrl = new URL("http://localhost:4725/wd/hub");
+			URL remoteUrl = new URL("http://localhost:4723/wd/hub");
 			 driver = new AndroidDriver(remoteUrl, desiredCapabilities);
 			user= new UserActions(driver);
 			
 			
 		}
 
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
 	
 	
 	@BeforeClass
-	public void login() throws IOException {
-//		user.pass("Application launche successully", driver);
-		   user.Click(pageObjects.userName);
-		    user.SendKeys(pageObjects.userName, "company");
-		    user.Click(pageObjects.password);
-		    user.SendKeys(pageObjects.password, "company");
-  		    user.Click(pageObjects.loginButton);
-  		    
-  		    
+	public void loadDatabase() throws IOException {
+		user.Click(pageObjects_Mohan.clikCheckatabase);
 	}
 	
 	
 	
 	@Test(dataProvider = "dp")
-	public void sampleTest(String mobileNumber, String name) throws IOException {
-		
-		user.pass("Loin successul", driver);
-		
-	    user.Click(pageObjects.makePaymentButton);
+	public void Wordweb(String searchText, String wordType) throws IOException {
+		System.out.println("Enter");
+
 	    
-	    user.SendKeys(pageObjects.phoneTextField, mobileNumber);
-	    user.SendKeys(pageObjects.nameTextField, name);
-		if (accessKey == null) {
-			TouchAction action = new TouchAction(driver);
-			action.press(PointOption.point(386, 718)).moveTo(PointOption.point(506, 718)).release().perform();
+	    
+		
+		user.ClearAndSendKeys(pageObjects_Mohan.searchText, searchText);
+		user.Click(pageObjects_Mohan.clickFirstResult);
+		
+		user.pass("Tetxt search result", driver);
+		user.Click(pageObjects_Mohan.bookMark);
+		user.Click(pageObjects_Mohan.backButton);
+		
+		
+		user.Click(pageObjects_Mohan.searchTab);
+		System.out.println(wordType);
+		
+//		System.out.println(Integer.parseInt( wordType.split(".")[0]));
+		
+		
+		switch (Integer.parseInt( wordType)) {
+		case 1:
+			user.Click(pageObjects_Mohan.noun);
+			break;
+		case 2:
+			user.Click(pageObjects_Mohan.verb);
+			break;
+		case 3:
+			user.Click(pageObjects_Mohan.adverb);
+			break;
+		case 4:
+			user.Click(pageObjects_Mohan.adjuctive);
+			break;
 
-		} else {
-			TouchAction action = new TouchAction(driver);
-			action.press(PointOption.point(390, 736)).moveTo(PointOption.point(506, 736)).release().perform();
+		default:
+			user.Click(pageObjects_Mohan.noun);
 		}
+		
+		
+		
+		
+		user.ClearAndSendKeys(pageObjects_Mohan.searchText, searchText);
+		
+		driver.pressKey(new KeyEvent(AndroidKey.ENTER));
+		
+		user.Click(pageObjects_Mohan.clickFirstResultType);
+		
+		user.pass("Tetxt search result", driver);
+		user.Click(pageObjects_Mohan.bookMark);
+		user.Click(pageObjects_Mohan.backButton);
+		user.Click(pageObjects_Mohan.lookup);
+		  
 
-		  user.Click(pageObjects.country);
-		  user.Click(pageObjects.India);
-		  
-		  user.Click(pageObjects.sendPaymentButton);
-		  user.Click(pageObjects.confirmationButton);
-		  
-		  user.pass("Payment successul", driver);
 
 	}
 	
@@ -138,20 +168,14 @@ public class EriBank  {
 	  @DataProvider
 	  public Object[][] dp() {
 		  
-			return ExcelUtil.getTestData("./testData.xlsx", "data")  ;
-//		    return new Object[][] {
-//	      new Object[] { "9786986811", "mohan" },
-//	      new Object[] { "9786986812", "Goutham" },
-//	      new Object[] { "9786986813", "Kevin" },
-//	      
-//	    };
+			return ExcelUtil.getTestData("./testData.xlsx", "Mohan")  ;
+
 	  }
 	
 	
 	@AfterClass
 	public void logout() throws IOException {
-		user.Click(pageObjects.Logout);
-		user.pass("Logout successul", driver);
+
 	}
 
 	@AfterTest
